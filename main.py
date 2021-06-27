fa891bf079a67b73983a423e3e13329d553f0618
Dev5H0 <thorhallur30102004@gmail.com>
2021-06-27 08:17:04 +0000

    Added "start.bat"

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools as iter 
 from setup import arr, exit
+from os import system
 
 x = [1,2,3,'Hello','World','Test','5H0']
 
@@ -20,11 +21,13 @@ for n1 in range(0, len1):
    for n2 in range(0, len2):
       for n3 in range(0, len3):
          x = x+(f'{arr.p1[n2]} {arr.p2[n3]} {arr.races[n1]}\n')
+         print(f'{arr.p1[n2]} {arr.p2[n3]} {arr.races[n1]}')
 
 with ff() as f:
    f.write(x)
 
-
 if __name__ == '__main__':
+   print('\n(This closes the window.)')
+   system('pause')
    f.close()
    exit()
